4d18c49477585f7e8a699e8433283fdbee631dd1
Rahul Kumar <rkrahul181997@gmail.com>
2024-06-03 12:01:18 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
 // import './App.css';
-import Country from "./country";
+import CitySelector from "./country";
 function App() {
   return (
     <div className="App">
-      <Country/>
+      <CitySelector/>
     </div>
   );
 }
